f9885cb8a38f33c9f9fa80a44ec856187d8f5cd4
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2016-10-27 09:42:32 -0400

    Hotfix for seqware 1.1.1-gsi release to remove linking of fastq to sample and lane

--- workflow-casava/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
+++ workflow-casava/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
@@ -209,7 +209,7 @@ public class WorkflowClient extends OicrWorkflow {
                     generateOutputFilename(flowcell, laneNum, p.getIusSwAccession(), p.getSampleName(), p.getBarcode(), "1", p.getGroupId()),
                     "chemical/seq-na-fastq-gzip",
                     manualOutput);
-            r1.setParentAccessions(Arrays.asList(p.getLaneSwAccession(), p.getIusSwAccession()));
+            r1.setParentAccessions(Arrays.asList(p.getIusSwAccession()));
             job.addFile(r1);
 
             if (readEnds > 1) {
@@ -220,7 +220,7 @@ public class WorkflowClient extends OicrWorkflow {
                         generateOutputFilename(flowcell, laneNum, p.getIusSwAccession(), p.getSampleName(), p.getBarcode(), "2", p.getGroupId()),
                         "chemical/seq-na-fastq-gzip",
                         manualOutput);
-                r2.setParentAccessions(Arrays.asList(p.getLaneSwAccession(), p.getIusSwAccession()));
+                r2.setParentAccessions(Arrays.asList(p.getIusSwAccession()));
                 job.addFile(r2);
             }
 
