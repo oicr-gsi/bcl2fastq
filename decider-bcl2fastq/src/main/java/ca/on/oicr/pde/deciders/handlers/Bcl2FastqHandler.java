dd68e1afe81b49ead4ded807f4fc73d162db6365
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2017-03-07 16:12:43 -0500

    Add missing imports

--- decider-bcl2fastq/src/main/java/ca/on/oicr/pde/deciders/handlers/Bcl2FastqHandler.java
+++ decider-bcl2fastq/src/main/java/ca/on/oicr/pde/deciders/handlers/Bcl2FastqHandler.java
@@ -10,6 +10,7 @@ import ca.on.oicr.pde.deciders.WorkflowRunV2;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -18,6 +19,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  *
